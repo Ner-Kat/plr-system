21efea051d267bc5dd57ea5325286e28bc9270ec
Ner-Kat <yaroslav446@gmail.com>
2021-05-28 10:52:53 +0300

    Разработана базовая функциональность контроллеров API.

--- PlrAPI/Startup.cs
+++ PlrAPI/Startup.cs
@@ -48,7 +48,7 @@ namespace PlrAPI
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "PlrAPI v1.0");
             });
 
             app.UseEndpoints(endpoints =>
